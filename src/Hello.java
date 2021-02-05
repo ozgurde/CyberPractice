2452e6e4eba18e8ba18cf0e317689ebc9a69c441
Ozgur <ozgurde8@gmail.com>
2021-02-06 01:01:38 +0200

    project is shared already in remote repo, committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,8 @@
 public class Hello {
     //more stuff
     //Mac users and windows users are happy to see commit dialog finally
+
+    //I have already published on Github.com
+    //now I have made more commits
+    //I dont share again but commit & push only!!
 }
